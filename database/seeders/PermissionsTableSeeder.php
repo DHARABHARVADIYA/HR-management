6f1589dc5616497cef230ec37ab17b906226810e
DHARABHARVADIYA <dharabharvadiya@gmail.com>
2024-11-29 16:22:12 +0530

    Add files via upload

--- database/seeders/PermissionsTableSeeder.php
+++ database/seeders/PermissionsTableSeeder.php
@@ -14,5 +14,10 @@ class PermissionsTableSeeder extends Seeder
         Permission::create(['name' => 'Update Department', 'module' => 'department']);
         Permission::create(['name' => 'Delete Department', 'module' => 'department']);
         // Add more permissions as needed
+
+        Permission::create(['name' => 'View Subdepartment', 'module' => 'subdepartment']);
+        Permission::create(['name' => 'Create Subdepartment', 'module' => 'subdepartment']);
+        Permission::create(['name' => 'Update Subdepartment', 'module' => 'subdepartment']);
+        Permission::create(['name' => 'Delete Subdepartment', 'module' => 'subdepartment']);
     }
 }

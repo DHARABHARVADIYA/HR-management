bada3dadc8f9449ab0f92243e7479bec16e6b98d
DHARABHARVADIYA <dharabharvadiya@gmail.com>
2024-11-29 16:23:24 +0530

    Add files via upload

--- resources/views/users/editRoles.blade.php
+++ resources/views/users/editRoles.blade.php
@@ -15,6 +15,7 @@
             @csrf
             @method('PUT')
 
+            <!-- Department Permissions Section -->
             <div class="form-group">
                 <label for="permissions">Select Permissions for Department</label>
                 <table class="table table-bordered">
@@ -22,16 +23,44 @@
                         <tr>
                             <th>Select</th>
                             <th>Permission</th>
+                            <th>Module</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach($permissions as $permission)
+                        @foreach($permissions->where('module', 'department')->take(4) as $permission)  <!-- Limit to 4 -->
                             <tr>
                                 <td>
                                     <input type="checkbox" name="permissions[]" value="{{ $permission->id }}"
                                            @if(in_array($permission->id, $userPermissions)) checked @endif>
                                 </td>
                                 <td>{{ $permission->name }}</td>
+                                <td>{{ ucfirst($permission->module) }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+
+            <!-- Subdepartment Permissions Section -->
+            <div class="form-group">
+                <label for="permissions">Select Permissions for Subdepartment</label>
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Select</th>
+                            <th>Permission</th>
+                            <th>Module</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($permissions->where('module', 'subdepartment')->take(4) as $permission)  <!-- Limit to 4 -->
+                            <tr>
+                                <td>
+                                    <input type="checkbox" name="permissions[]" value="{{ $permission->id }}"
+                                           @if(in_array($permission->id, $userPermissions)) checked @endif>
+                                </td>
+                                <td>{{ $permission->name }}</td>
+                                <td>{{ ucfirst($permission->module) }}</td>
                             </tr>
                         @endforeach
                     </tbody>
